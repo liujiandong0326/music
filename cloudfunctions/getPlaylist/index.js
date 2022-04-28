646905f2a07d32995e571794d3ba68cff9e1da5c
liujd2 <liujd2@lenovo.com>
2022-04-28 10:15:23 +0800

    feat: 优化歌单插入数据

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -8,6 +8,8 @@ cloud.init({
 
 const URL = "https://apis.imooc.com/personalized?icode=07B5773D5F5F47E1";
 const db = cloud.database();
+const playlistCollection = db.collection("playlist");
+const MAX_LIMIT = 100;
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -16,15 +18,51 @@ exports.main = async (event, context) => {
   if (data.code === 200) {
     const playlist = data.result;
     if (playlist.length) {
+      // const list = await playlistCollection.get(); // 最多可以获取 100 条
+      const countResult = await playlistCollection.count();
+      const total = countResult.total;
+      const requestNum = Math.ceil(total / MAX_LIMIT);
+      const tasks = [];
 
-      for (let i = 0; i < playlist.length; i++) {
-        const item = playlist[i];
-        item.createTime = db.serverDate()
+      for (let i = 0; i < requestNum; i++) {
+        // skip: 当前从第几条开始取
+        // limit: 取的条数
+        tasks.push(
+          playlistCollection
+            .skip(i * MAX_LIMIT)
+            .limit(MAX_LIMIT)
+            .get()
+        );
       }
-      await db
-        .collection("playlist")
+      let list = {
+        data: [],
+      };
+      if (tasks.length) {
+        list = await (await Promise.all(tasks)).reduce((prev, current) => ({
+          data: prev.data.concat(current.data),
+        }));
+      }
+
+      // 去重：如果数据库里边已经有相同的数据则不要插入
+      const newData = [];
+      for (let i = 0; i < playlist.length; i++) {
+        let flag = true;
+        for (let j = 0; j < list.data.length; j++) {
+          if (playlist[i].id === list.data[j].id) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          playlist[i].createTime = db.serverDate();
+          newData.push(playlist[i]);
+        }
+      }
+
+      if(newData.length) {
+        await playlistCollection
         .add({
-          data: [...playlist],
+          data: [...newData],
         })
         .then((res) => {
           console.log("插入成功");
@@ -33,8 +71,10 @@ exports.main = async (event, context) => {
           console.log(err);
           console.error("插入失败");
         });
+      }
+      return newData.length;
     }
   } else {
-    console.log(data.msg);
+    console.error(data.msg);
   }
 };
